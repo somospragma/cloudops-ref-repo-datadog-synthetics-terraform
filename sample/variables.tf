3c3146297d38065d2e70a1f43b6bb501f23a74f0
Cristian Noguera <127320165+cristiannoguera11@users.noreply.github.com>
2025-02-17 11:45:30 -0500

    Update variables.tf

--- sample/variables.tf
+++ sample/variables.tf
@@ -1,54 +1,82 @@
-#################### Variables Datadog ####################
-variable "datadog_api_key" {
-  type      = string
-}
-
-variable "datadog_app_key" {
-  type      = string
-}
-
-variable "datadog_api_url" {
-  type      = string
-}
-
-variable "datadog_notification_channel" {
-  type = string
-}
-
-#################### Variables Synthetics ####################
-variable "synthetics_config" {
-  description = "List of Datadog Synthetic Test configurations"
-  type = list(object({
-    name                     = string
-    url                      = string
-    type                     = string
-    subtype                  = string
-    status                   = string
-    notification_channel     = string
-    locations                = list(string)
-    tags                     = list(string)
-    method                   = string
-    request_headers_content_type = string
-    assertions               = list(object({
-      type     = string
-      operator = string
-      target   = string
-    }))
-    tick_every               = number
-    retry_count              = number
-    retry_interval           = number
-    monitor_renotify_interval = number
-  }))
-}
-
-#################### Variables AWS ####################
+#############################################################
+#################### Variables Generales ####################
+#############################################################
 
 variable "project" {
   description = "Nombre del proyecto"
   type        = string
+  default     = "Observability"
 }
 
 variable "environment" {
   description = "Ambiente de la carga de trabajo"
   type        = string
+  default     = "Monitoring"
+}
+
+variable "client" {
+  description = "Nombre del cliente propietario de la aplicación"
+  type        = string
+  default     = "Pragma"
+
+}
+
+variable "datadog_api_key" {
+  type      = string
+  sensitive = true
+  nullable  = false
+  default   = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
+
+variable "datadog_app_key" {
+  type      = string
+  sensitive = true
+  nullable  = false
+  default   = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
+
+variable "datadog_api_url" {
+  type      = string
+  sensitive = true
+  nullable  = false
+  default   = "https://api.datadoghq.com/"
+}
+
+###########################################################################
+#################### Variables Synthetics Browser Test ####################
+###########################################################################
+
+variable "synthetics_browser_config" {
+  description = "List of Datadog Synthetic Browser Test configurations"
+  type = list(object({
+    name       = string
+    type       = string
+    status     = string
+    message    = string
+    device_ids = list(string)
+    locations  = list(string)
+    tags       = list(string)
+    request_definition = object({
+      method = string
+      url    = string
+    })
+    browser_step = list(object({
+      name = string
+      type = string
+      params = list(object({
+        check = string
+        value = string
+      }))
+    }))
+    browser_variable = list(object({
+      type    = string
+      id = string
+      name    = string
+      pattern = string
+      secure = bool
+    }))
+    options_list = object({
+      tick_every = number
+    })
+  }))
 }
