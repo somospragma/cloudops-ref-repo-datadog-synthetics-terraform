7265549bbe900b789b0ee01b77c4805be16a81b7
Cristian Noguera <127320165+cristiannoguera11@users.noreply.github.com>
2025-07-17 09:51:51 -0500

    Update synthetics.tf

--- sample/synthetics.tf
+++ sample/synthetics.tf
@@ -1,40 +1,50 @@
-resource "datadog_synthetics_test" "test_uptime" {
- # for_each = { for index, item in var.synthetics_config : index => item }
-  for_each = { for item in var.synthetics_config : item.name => item }
-  name      = "${var.project}-${var.environment}-${each.value.name}"
-  type      = each.value.type
-  subtype   = each.value.subtype
-  status    = each.value.status
-  message   = each.value.notification_channel
-  locations = each.value.locations
-  tags      = concat(["Account-AWS", var.project, var.environment], each.value.tags)
+#######################################################################
+################### Datadog Synthetics Browser Test ###################
+#######################################################################
+
+resource "datadog_synthetics_test" "browser_test" {
+  for_each = { for item in var.synthetics_browser_config : item.name => item }
+  name     = "${var.client}-${var.project}-${var.environment}-${each.value.name}"
+  type     = each.value.type
+  status   = each.value.status
+  #message    = each.value.notification_channel
+  device_ids = ["laptop_large"]
+  locations  = each.value.locations
+  tags       = concat([var.project, var.environment], each.value.tags)
 
   request_definition {
-    method = each.value.method
-    url    = each.value.url
+    method = each.value.request_definition.method
+    url    = each.value.request_definition.url
   }
 
-  request_headers = {
-    Content-Type = each.value.request_headers_content_type
-  }
-
-dynamic "assertion" {
-    for_each = each.value.assertions
+  dynamic "browser_step" {
+    for_each = each.value.browser_step
     content {
-      type     = assertion.value.type
-      operator = assertion.value.operator
-      target   = assertion.value.target
+      name = browser_step.value.name
+      type = browser_step.value.type
+
+      dynamic "params" {
+        for_each = browser_step.value.params
+        content {
+          check = params.value.check
+          value = params.value.value
+        }
+      }
+    }
+  }
+
+  dynamic "browser_variable" {
+    for_each = each.value.browser_variable
+    content {
+      type    = browser_variable.value.type
+      id      = browser_variable.value.id
+      name    = browser_variable.value.name
+      pattern = browser_variable.value.pattern
+      secure  = browser_variable.value.secure
     }
   }
 
   options_list {
-    tick_every = each.value.tick_every
-    retry {
-      count    = each.value.retry_count
-      interval = each.value.retry_interval
-    }
-    monitor_options {
-      renotify_interval = each.value.monitor_renotify_interval
-    }
+    tick_every = each.value.options_list.tick_every
   }
 }
